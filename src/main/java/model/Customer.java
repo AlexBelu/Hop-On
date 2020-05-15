ef38a2e837b5b9b413da59e5ff6c45198b334d05
Alexandru Belu <alexandru_belu5@yahoo.com>
2020-05-15 15:13:28 +0300

    [HO-24] Create a method to add the bought ticket to his list.

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,8 +1,12 @@
 package model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import model.Flight;
 import model.User;
+import services.JSONWriterFlights;
+import services.UserService;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Customer extends User {
@@ -12,4 +16,22 @@ public class Customer extends User {
         super(username,password,"Customer");
     }
     public Customer(){}
+    public void addFlight(int flightNo, ArrayList<Flight> flight_list) {
+        Flight a = null;
+        for (Flight flight : flight_list) {
+            if (flightNo == flight.getFlightNo()) {
+                this.myFlights.add(flight);
+                a=flight;
+            }
+        }
+        flight_list.remove(a);
+        try {
+            ObjectMapper objectMapper1 = new ObjectMapper();
+            objectMapper1.writerWithDefaultPrettyPrinter().writeValue(UserService.getPathCustomer().toFile(), UserService.getPilots());
+            ObjectMapper objectMapper2 = new ObjectMapper();
+            objectMapper2.writerWithDefaultPrettyPrinter().writeValue(JSONWriterFlights.getPathFlight().toFile(), UserService.getFlights());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
