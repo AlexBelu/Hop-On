5bc1a0639a82fa70a1650df832d39e7592e60be1
Liana Aruncutean <liana.aruncutean@gmail.com>
2020-05-16 09:54:30 +0300

    [HO-21] Create the GUI for the list of flights.

--- src/main/java/view/CustomerViewFlights.java
+++ src/main/java/view/CustomerViewFlights.java
@@ -1,11 +1,14 @@
 package view;
 
+import services.UserService;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class CustomerViewFlights extends JFrame {
     CustomerViewFlights(){
         super("View Flights");
-        JLabel welcome = new JLabel("Welcome to the 'View Flights' Page");
+        JLabel welcome = new JLabel("Welcome to the 'Show Flights' Page");
         setSize(500,500);
         setLocation(500,280);
         welcome.setBounds(50,10,200,60);
@@ -14,10 +17,18 @@ public class CustomerViewFlights extends JFrame {
         getContentPane().add(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    }
-    public static void main(String[] args) {
-        CustomerAdd frameTabel = new CustomerAdd();
-        frameTabel.setVisible(true);
-    }
+        String[] b = UserService.findCustomer(LoginView.getTxtUsername()).showFlights();
 
+        if (b != null) {
+            JList a = new JList(b);
+            a.setBounds(100, 100, 200, 60);
+            panel.add(a);
+            add(new JScrollPane(panel), BorderLayout.CENTER);
+        } else {
+            JLabel er = new JLabel("You do not have any flights", SwingConstants.CENTER);
+            er.setVerticalAlignment(SwingConstants.CENTER);
+            er.setBounds(180, 200, 250, 100);
+            panel.add(er);
+        }
+    }
 }
