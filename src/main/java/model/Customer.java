25aed31225426e67e2aa16f881432ae8dd49ad01
Liana Aruncutean <liana.aruncutean@gmail.com>
2020-05-20 20:26:53 +0300

    [HO-29] Create a method to add the selected flight to the boarding cards list.

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -16,11 +16,31 @@ public class Customer extends User {
         super(username,password,"Customer");
     }
     public Customer(){}
+
     public void addFlight(int flightNo, ArrayList<Flight> flight_list) {
         Flight a = null;
         for (Flight flight : flight_list) {
             if (flightNo == flight.getFlightNo()) {
                 this.myFlights.add(flight);
+                a=flight;
+            }
+        }
+        flight_list.remove(a);
+        try {
+            ObjectMapper objectMapper1 = new ObjectMapper();
+            objectMapper1.writerWithDefaultPrettyPrinter().writeValue(UserService.getPathCustomer().toFile(), UserService.getCustomers());
+            ObjectMapper objectMapper2 = new ObjectMapper();
+            objectMapper2.writerWithDefaultPrettyPrinter().writeValue(JSONWriterFlights.getPathFlight().toFile(), UserService.getFlights());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addBoardingCards(int flightNo, ArrayList<Flight> flight_list) {
+        Flight a = null;
+        for (Flight flight : flight_list) {
+            if (flightNo == flight.getFlightNo()) {
+                this.boardingCard.add(flight);
                 a=flight;
             }
         }
