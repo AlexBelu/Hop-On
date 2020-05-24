881b383f375d68e6f990e61a00e0f8496325a917
Liana Aruncutean <liana.aruncutean@gmail.com>
2020-05-24 14:52:06 +0300

    [HO-38] Move project resources.

--- src/main/java/services/FileSystemService.java
+++ src/main/java/services/FileSystemService.java
@@ -8,6 +8,6 @@ public class FileSystemService {
     private static final String USER_FOLDER = System.getProperty("user.dir");
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER);
     public static Path getPathToFile(String... path) {
-        return APPLICATION_HOME_PATH.resolve(Paths.get(".", path));
+        return APPLICATION_HOME_PATH.resolve(Paths.get("src/main/resources", path));
     }
 }
